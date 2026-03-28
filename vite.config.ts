6ebc5d7cd4a22d6b21f182333e2dc74ade5adac5
Amoskouassi <114862059+Amoskouassi@users.noreply.github.com>
2026-03-28 06:39:41 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,18 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    middlewareMode: true,
-    headers: {
-      'Content-Type': 'application/javascript; charset=utf-8'
+  build: {
+    target: ['es2015', 'chrome58', 'firefox57', 'safari11', 'edge18'],
+    outDir: 'dist',
+    minify: 'terser',
+    cssCodeSplit: false,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      }
     }
+  },
+  server: {
+    port: 3000,
   }
 })
